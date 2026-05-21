4a95a3938f7c5fc7c84e467a38f05acb846a8135
aayad <ayadabdellah08@gmail.com>
2026-05-21 23:34:07 +0100

    add the config file

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -4,7 +4,9 @@ set -e
 
 mkdir -p /etc/nginx/ssl
 
-openssh req -x509 -nodes -days 365 -newkey rsa:2048\
-    -keyout /etc/nginx/ssl/nginx.key\
-    -out /etc/nginx/ssl/nginx.crt\
-    -subj
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/nginx/ssl/nginx.key \
+    -out /etc/nginx/ssl/nginx.crt \
+    -subj "/CN=${DOMAIN_NAME}" >/dev/null 2>/dev/null
+
+exec "$@"
