13db9ba0bdb500814d88d60bec4c6931a19260c3
aayad <ayadabdellah08@gmail.com>
2026-05-21 19:24:34 +0100

    EGNORE THE .ENV

---
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p /etc/nginx/ssl
+
+openssh req -x509 -nodes -days 365 -newkey rsa:2048\
+    -keyout /etc/nginx/ssl/nginx.key\
+    -out /etc/nginx/ssl/nginx.crt\
+    -subj
