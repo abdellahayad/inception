38b3801b80e651ce076a2528381ad6f038decfbb
aayad <aayad@debian.debian>
2026-06-08 12:04:03 -0400

    fix problems in script

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -4,22 +4,15 @@ set -e
 DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password.txt)
 DB_PASSWORD=$(cat /run/secrets/db_password.txt)
 
-mysql_install_db --user=mysqp --datadir=/var/lib/mysql
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-mysql --user=mysql --bootstrap << EOF
-
+mysqld --user=mysql --bootstrap << EOF
 FLUSH PRIVILEGES;
-
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
-
-CREATE DATABASE IF NOT EXISTS 'MYSQL_DATABASE'
-
-CREATE USER IF NOT EXISTS 
-
+CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-
 GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-exec mysql --user=mysql
+exec mysqld --user=mysql
