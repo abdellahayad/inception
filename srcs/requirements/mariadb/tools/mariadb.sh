5b152bf44498bc7e87751a493118c8668a7ff759
aayad <aayad@debian.debian>
2026-06-08 11:50:54 -0400

    i finished the mariadb script

---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password.txt)
+DB_PASSWORD=$(cat /run/secrets/db_password.txt)
+
+mysql_install_db --user=mysqp --datadir=/var/lib/mysql
+
+mysql --user=mysql --bootstrap << EOF
+
+FLUSH PRIVILEGES;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
+
+CREATE DATABASE IF NOT EXISTS 'MYSQL_DATABASE'
+
+CREATE USER IF NOT EXISTS 
+
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+
+GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+exec mysql --user=mysql
