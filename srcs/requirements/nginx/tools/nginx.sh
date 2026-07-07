a69230f0c8b6bac5540179ac5b0096c1bac17940
aayad <ayadabdellah08@gmail.com>
2026-07-07 19:28:09 +0100

    i change the always by on-failure in docker compose

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -3,10 +3,11 @@
 set -e
 
 mkdir -p /etc/nginx/ssl
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/nginx/ssl/nginx.key \
-    -out /etc/nginx/ssl/nginx.crt \
-    -subj "/CN=${DOMAIN_NAME}" >/dev/null 2>/dev/null
+if [ ! -f /etc/nginx/ssl/nginx.key ]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout /etc/nginx/ssl/nginx.key \
+        -out /etc/nginx/ssl/nginx.crt \
+        -subj "/CN=${DOMAIN_NAME}" >/dev/null 2>/dev/null
+fi
 
 exec "$@"
